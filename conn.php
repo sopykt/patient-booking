9ee4f5bee3099d008bd17320ab68c8baefe6a23b
Luxing Huang <steven@accsc.im>
2015-04-03 15:04:32 -0300

    database check

--- conn.php
+++ conn.php
@@ -1,18 +1,26 @@
 <?php
 $host = "db.luxing.im";
-$dbusername = "a6";
+$dbusername = "a6-test";
 $dbpassword = "JtsTuDe6SFKVxUvF";
-$dbname = "a6";
+$dbname = "a6-test";
 
 $db = mysqli_connect($host, $dbusername, $dbpassword, $dbname);
 if ($db->connect_error) {
     die("Connection Failed". $db->connect_error);
 }
 
+function install() {
+    # Import SQL into the database.
+    global $db;
+}
+
 function isInstalled() {
+    global $db;
     $sql = "SELECT * FROM puser";
-    $db->query($sql);
-    var_dump($db);
+    $result = $db->query($sql);
+    if ($result == NULL) {
+        install();
+    }
 }
 
 isInstalled();
